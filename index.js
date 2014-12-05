15469f2e87ee0c9ca865930b5e1985ff8d72a0ee
samsuanchen <samsuanchen@gmail.com>
2014-12-05 16:42:28 +0800

    1st commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var boot=require("boot");
+boot("_2048","main","main");
---
+++ ksana.js
@@ -0,0 +1,36 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "_2048",
+ "date": "2014-12-05T08:31:35.856Z",
+ "minruntime": 4,
+ "baseurl": "http://rawgit.com/samsuanchen/_2048/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "build.js",
+  "build.css",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "_2048.kdb"
+ ],
+ "filesizes": [
+  422,
+  502870,
+  132383,
+  277980,
+  644338,
+  522,
+  1191
+ ],
+ "filedates": [
+  "2014-12-05T08:28:17.000Z",
+  "2014-12-05T08:29:35.000Z",
+  "2014-12-05T08:29:35.000Z",
+  "2014-12-05T08:28:17.000Z",
+  "2014-12-05T08:28:17.000Z",
+  "2014-12-05T08:29:34.000Z",
+  "2014-12-05T08:30:36.000Z"
+ ]
+})
