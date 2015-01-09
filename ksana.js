4a8933c4176f6748af6fe941d430f99d0ece441d
samsuanchen <samsuanchen@gmail.com>
2015-01-09 22:00:09 +0800

    test

--- ksana.js
+++ ksana.js
@@ -2,7 +2,7 @@ jsonp_handler({
  "version": "1",
  "build": 162,
  "title": "_2048",
- "date": "2015-01-01T11:48:23.226Z",
+ "date": "2015-01-09T11:48:23.226Z",
  "minruntime": 4,
  "baseurl": "http://rawgit.com/samsuanchen/_2048/master/",
  "description": "",
